df09561718e540f49785fbac8ed1ebcc050c9e02
zergend <zergend@gmail.com>
2016-05-13 16:01:14 +0300

    доделал на 99%

--- js/sedona.js
+++ js/sedona.js
@@ -1,3 +1,23 @@
+// ПРОЯВЛЯЮЩАЯСЯ ФОРМА
+var link = document.querySelector(".search-form-open");
+var popup = document.querySelector(".modal-content");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+    }
+  }
+});
+
+
+// ИНТЕРАКТИВНАЯ КАРТА
+
 var myMap;
 
 // Дождёмся загрузки API и готовности DOM.
